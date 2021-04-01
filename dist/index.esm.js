1464f11a60b1eeec30868ded86700d409b6148b7
ZombieDefi <zombiedefi@gmail.com>
2021-04-01 02:13:54 -0300

    Logo update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1826,7 +1826,7 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     _a.isDark; var props = __rest(_a, ["isDark"]);
-    return (React.createElement(Svg, __assign({ xmlns: "http://www.w3.org/2000/svg", width: "136", height: "34", viewBox: "0 0 136 34" }, props),
+    return (React.createElement(Svg, __assign({ xmlns: "http://www.w3.org/2000/svg", width: "180", height: "34", viewBox: "0 0 180 34" }, props),
         React.createElement("defs", null,
             React.createElement("filter", { id: "u47apuouga" },
                 React.createElement("feColorMatrix", { in: "SourceGraphic", values: "0 0 0 0 0.219608 0 0 0 0 0.541176 0 0 0 0 0.968627 0 0 0 1.000000 0" }))),
@@ -1834,11 +1834,11 @@ var Logo = function (_a) {
             React.createElement("g", null,
                 React.createElement("g", null,
                     React.createElement("text", { fill: "#414859", "font-size": "20", "font-weight": "500", transform: "translate(-56 -16) translate(56 16)" },
-                        React.createElement("tspan", { x: "38", y: "24", style: { fontFamily: "Lucida Console" }, fontSize: 35 },
+                        /* React.createElement("tspan", { x: "38", y: "24", style: { fontFamily: "Lucida Console" }, fontSize: 35 },
                             "Z",
                             ""),
-                        "   ",
-                        React.createElement("tspan", { x: "63.5", y: "24", "font-family": "Lucida Console" }, "- Defi")),
+                        "   ", */
+                        React.createElement("tspan", { x: "38", y: "24", "font-family": "Lucida Console" }, "Zombie Defi")),
                     React.createElement("g", null,
                         React.createElement("g", { fill: "#932C2C" },
                             React.createElement("path", { d: "M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z", transform: "translate(2.833 2.833)" })))))))
